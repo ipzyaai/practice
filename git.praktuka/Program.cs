987c8abd859bdd48d3e33c43fac839459ba02c18
Якусевич Анна Ігорівна <ipz234_yaai@student.ztu.edu.ua>
2025-01-30 14:13:49 +0200

    Initial commit

---
+++ git.praktuka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+
+            Console.Write("Введіть перше число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+            Console.WriteLine($"Сума чисел: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
